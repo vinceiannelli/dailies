b9bfb546e720741fff7987a58d2fe7ef13dcecd2
Vince Iannelli <viannelli@gmail.com>
2022-09-26 10:00:28 -0600

    it's hello world time

---
+++ 220926/hello-world.js
@@ -0,0 +1,8 @@
+// most common ways to print to console
+console.log('hello world!');
+console.error('stop this!');
+
+// less common below
+console.debug('debug');
+console.warn('warn');
+console.info('info');
