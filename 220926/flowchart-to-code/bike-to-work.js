11e32ab8db7c1b78b76ecf7286d0702bd55c8b5d
Vince Iannelli <viannelli@gmail.com>
2022-09-26 13:14:50 -0600

    added flowchart for biking to work

---
+++ 220926/flowchart-to-code/bike-to-work.js
@@ -0,0 +1,25 @@
+// flowchart conversion
+// using https://docs.google.com/presentation/d/1iLjQdr_lwVt-aOetHOZe4d5Rwo1bPAR_8Qkdhxw81uU
+//
+
+let isSnowOrIce = true; // do I need my winter bike?
+let isChildComing = false; // is Cedar coming?
+
+if (!isSnowOrIce) {
+    console.log('Get my summer bike.');
+} else {
+    console.log('Get my winter bike.');
+}
+
+console.log('Put on helmet.');
+
+if (isChildComing) {
+    console.log('Get Cedar sat and put on her helmet.');
+    console.log('Bike to daycare.');
+    console.log('Drop off Cedar at her classroom.');
+}
+
+console.log('Bike to library.');
+console.log('Take off helmet and lock bike.');
+
+
