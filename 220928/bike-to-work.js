89bbf3fd1f3822fc958a2b28ef58e6a51b5045a5
Vince Iannelli <viannelli@gmail.com>
2022-09-28 11:13:27 -0600

    added bike to work flowchart

---
+++ 220928/bike-to-work.js
@@ -0,0 +1,35 @@
+// flowchart conversion
+// using https://docs.google.com/presentation/d/1iLjQdr_lwVt-aOetHOZe4d5Rwo1bPAR_8Qkdhxw81uU
+//
+
+// let isSnowOrIce = true; // do I need my winter bike?
+// let isChildComing = false; // is Cedar coming?
+
+const bikeToWork = (isSnowOrIce, isChildComing) => {
+    if (!isSnowOrIce) {
+        console.log('Get my summer bike.');
+    } else {
+        console.log('Get my winter bike.');
+    }
+
+    console.log('Put on helmet.');
+
+    if (isChildComing) {
+        console.log('Get Cedar sat and put on her helmet.');
+        console.log('Bike to daycare.');
+        console.log('Drop off Cedar at her classroom.');
+    }
+
+    console.log('Bike to library.');
+    console.log('Take off helmet and lock bike.');
+}
+console.log('----');
+bikeToWork (true, true);
+console.log('----');
+bikeToWork (true, false);
+console.log('----');
+bikeToWork (false, true);
+console.log('----');
+bikeToWork (false, false);
+console.log('----');
+
