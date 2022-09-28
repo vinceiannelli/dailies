47b449fb36657e033712cca8fcc37c17b811b4c7
Vince Iannelli <viannelli@gmail.com>
2022-09-28 14:01:27 -0600

    separated into functions

--- 220928/bike-to-work.js
+++ 220928/bike-to-work.js
@@ -5,20 +5,33 @@
 // let isSnowOrIce = true; // do I need my winter bike?
 // let isChildComing = false; // is Cedar coming?
 
-const bikeToWork = (isSnowOrIce, isChildComing) => {
+const whichBike = (isSnowOrIce) => {
     if (!isSnowOrIce) {
         console.log('Get my summer bike.');
     } else {
         console.log('Get my winter bike.');
     }
+}
 
+const putOnHelmet = () => {
     console.log('Put on helmet.');
+}
 
+const daycare = (isChildComing) => {
     if (isChildComing) {
         console.log('Get Cedar sat and put on her helmet.');
         console.log('Bike to daycare.');
         console.log('Drop off Cedar at her classroom.');
     }
+}
+
+const bikeToWork = (isSnowOrIce, isChildComing) => {
+    
+    whichBike(isSnowOrIce);
+    
+    putOnHelmet();
+
+    daycare(isChildComing);
 
     console.log('Bike to library.');
     console.log('Take off helmet and lock bike.');
--- 220928/making-coffee-with-functions/6_arrays/objects.js
+++ 220928/making-coffee-with-functions/6_arrays/objects.js
@@ -9,7 +9,20 @@ const myObject = {
   sayHello: sayHelloFunc
 }
 
+const arrNames = ['jim', 'jam', 'jon'];
 
 const myObjectJSON = JSON.stringify(myObject)
 
+const arrNamesJSON = JSON.stringify(arrNames);
+
 console.log(myObjectJSON)
+console.log(myObject)
+console.log(arrNamesJSON)
+console.log(arrNames)
+
+
+const arr = [];
+
+const { fromWeb } = require("https://adventofcode.com/2020/day/2/input");
+console.log (fromWeb);
+
