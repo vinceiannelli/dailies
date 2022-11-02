d5d966ad96f0d2ece20f9571a70c9d7fd1411b80
Vince Iannelli <viannelli@gmail.com>
2022-11-02 10:44:55 -0600

    playing with react

--- 221024/async-fun/index.js
+++ 221024/async-fun/index.js
@@ -1,8 +1,18 @@
-function doSomething() {
-	setTimeout(() => {
-		console.log('do something else');
-	}, 1010);
-	console.log('do something');
+import fetch from 'node-fetch';
+import readline from 'readline-sync';
+
+const postalCode = readline.question('what is your postal code? \n').toUpperCase().trim().replaceAll(' ', '');
+console.log(postalCode);
+
+async function getPostCodeInfo() {
+	const response = await fetch(`https://represent.opennorth.ca/postcodes/${postalCode}/`);
+	const result = await response.json();
+	// console.log(result);
+	const reps = result.representatives_centroid;
+	console.log(`You are in ${result.city}, ${result.province}.`);
+
+	const mp = reps.find((r) => r.elected_office === 'MP');
+	console.log(`Your MP is ${mp.name}.`);
 }
 
-doSomething();
+getPostCodeInfo();
